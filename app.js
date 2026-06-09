7575a7170414104082b72280d5fa5fe8331e1395
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-09 17:23:12 +0530

    ES module and CommonJs

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require('./test.js');           //one module into another...
+
 var name = "Namaste Node JS"
 console.log(name);
 // console.log(globalThis);
 
-const {x, calculateSum} = require('./sum.js')
+const {x, calculateSum} = require ('./sum.js');
 let a= 10;
 let b =20;
 calculateSum(a,b)   
 
 console.log(x);
 
-console.log(this);      //empty object
+// console.log(this);      //empty object
 console.log(globalThis === global);     //true
+
+// In ES module we use import and export
+// In commonJS we use require and module.exports
