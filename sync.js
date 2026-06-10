783cc49ed0dee2f5037950d25ec382705719b8e7
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-10 17:19:48 +0530

    Synchronous task

---
+++ sync.js
@@ -0,0 +1,11 @@
+console.log("Namaste India !!");
+
+let a = 10756;
+let b = 12556;
+
+function multiply(a, b){
+    const result = a*b;
+    return result;
+}
+let c = multiply(a,b);
+console.log("result of the the func is: ", c);
